9881f2ee6fa9952a82ec50b8c367b10ecdba5988
ataasawafta <32885339+ataasawafta@users.noreply.github.com>
2017-11-06 14:44:28 +0200

    Update ExerciseDisplay.m

--- Calories Counter/ExerciseDisplay.m
+++ Calories Counter/ExerciseDisplay.m
@@ -80,7 +80,7 @@ self.navigationController.title=@"Exercise";
     UIAlertController * selectedcell=[UIAlertController alertControllerWithTitle:name message:[[NSString alloc]initWithFormat:@""] preferredStyle:UIAlertControllerStyleAlert];
     
     [selectedcell addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-        NSArray *r=[dbmanager loadDataFromDB:[[NSString alloc]initWithFormat:@"select * from Exercise where execriseName='%@' and email='%@'and datef='%@'",name,email,[MainScreen currentDate]]];
+        NSArray *r=[dbmanager loadDataFromDB:[[NSString alloc]initWithFormat:@"select * from Exercise where ExerciseName='%@' and email='%@'and datef='%@'",name,email,[MainScreen currentDate]]];
         textField.placeholder = r[0][2];
     }];
     
